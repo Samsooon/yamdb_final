ba607b6d7434e772e8b55838f2cecb62ecc29e8a
Dmitrii Samsonov <D.samsooon@vk.com>
2023-05-10 23:12:29 +0300

    ok

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -132,10 +132,10 @@ SIMPLE_JWT = {
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Mail server emulation
